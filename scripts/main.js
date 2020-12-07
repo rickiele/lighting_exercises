de0f82204c3b2f818010f7dd0266fef5d710e3fb
rickiele <rickienthefishies@gmail.com>
2020-12-06 21:11:16 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,64 @@
+// console.log("Welcome to your playground")
+
+/* Playground to try js in */
+
+// Exercise 1: November 19, 2020
+// Functions can return a value that we can capture:
+// Write a function that takes a string of a dog breed as an argument (like 'border collie')
+// Have the function return "my favorite dog breed is" plus the passed in string. If no argument is passed to the function, return "I like cats".
+// Execute the function in a way that captures the returned value in a variable.
+// Console.log the string "When it comes to pets," plus the returned value of the function.
+
+// const dogString = (dog) => {
+//     if (dog) {
+//     return `my favorite dog breed is ${dog}`
+//     }
+//     return "I like cats"
+// }
+
+// const dogBreed = dogString("border collie")
+// console.log(`When it comes to pets, ${dogBreed}.`)
+
+
+// Skeleton of a for loop
+// for (initialExpression; condition; incrementExpression) {
+//   (initializing i to 0/ loop to 0; compare the value of i to something else; increment operator to increment the value of i with 1;)
+//   statement
+// }
+
+  // for (let i = 5; i >= 1; i--) {
+  //   if (i % 2 !== 0) console.log(i);
+  // }
+
+
+  // Exercise 2: November 23, 2020
+  // let painter = {
+  //   tools: ["brush", "roller", "caulk", "sandpaper", "dropcloth"],
+  //   uniform: "white overalls",
+  //   cost_per_hour: 25.00,
+  //   insured: true
+  // };
+
+  // painter.dancemove = () => "moonwalk";
+
+  // console.log(`The painter's signature dance move is the ${painter.dancemove()}.`);
+
+  // console.log(painter);
+
+  // Add an action that the painter can perform ( a property whose value is a function ).
+  // Have her take that action by execucting the method you created
+
+
+  //Exercise: Event Listener
+// const button = document.querySelector("#btn--dontclick")
+
+// button.addEventListener("click", () => {
+//   console.log("The uncooperative user has clicked the button.")
+//   alert("You weren't supposed to click this button.")
+// })
+
+
+//Mosh - Exercise 1
+//
+
+
